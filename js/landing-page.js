6b9bc22cc198f297ecf344c30f33856eac740380
HABEEB AWOYEMI <awoyemi.habeeb@gmail.com>
2021-06-21 18:51:37 +0100

    Created Landing page

---
+++ js/landing-page.js
@@ -0,0 +1,6 @@
+function openNav() {
+  document.querySelector('.landing-nav')?.classList?.add('open')
+}
+function closeNav() {
+  document.querySelector('.landing-nav')?.classList?.remove('open')
+}
