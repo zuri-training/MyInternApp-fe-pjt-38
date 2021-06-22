f2530f46d59cbfdc511163e2ec81d2bf9f5d7487
HABEEB AWOYEMI <awoyemi.habeeb@gmail.com>
2021-06-22 13:09:36 +0100

    updated readme guide and added landing page

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function openNav() {
+  document.querySelector('.landing-nav')?.classList?.add('open')
+}
+function closeNav() {
+  document.querySelector('.landing-nav')?.classList?.remove('open')
+}
+function signupLoginBtn() {
+  document.querySelector('.landing-nav')?.classList?.add('open');
+}
+function signupLoginBtn() {
+  document.querySelector('.landing-nav')?.classList?.remove('open');
+}
